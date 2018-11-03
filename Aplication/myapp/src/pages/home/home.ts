c7a68ed4db267d0e48c7db641ee759332cb32658
andresfqm <andrescable@hotmail.com>
2018-11-03 11:59:05 -0500

    Para tener en cuenta

---
+++ Aplication/myapp/src/pages/home/home.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { UserServiceProvider } from '../../providers/user-service/user-service';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  users: any[] = [];
+  constructor(public navCtrl: NavController,
+    public userService: UserServiceProvider) {
+
+  }
+
+  ionViewDidLoad() {
+    this.userService.getUsers()
+      .subscribe(
+        (data) => { // Success
+          this.users = data['results'];
+        },
+        (error) => {
+          console.error(error);
+        }
+      )
+  }
+}
